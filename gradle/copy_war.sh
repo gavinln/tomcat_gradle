5618d787000cdd8e9d459cc2a8533a8b9e455e5b
Gavin Noronha <gavinln@hotmail.com>
2015-03-29 16:29:18 -0700

    Added example of using gradle to build war

---
+++ gradle/copy_war.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# call this with sudo
+CATALINA_HOME=/opt/apache-tomcat
+
+cp ../java/simple-war/simple.war $CATALINA_HOME/webapps
---
+++ gradle/set_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# use source set_env.sh to setup environment to use gradle
+
+export CATALINA_HOME=/opt/apache-tomcat
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
